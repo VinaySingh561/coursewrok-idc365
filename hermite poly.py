935dc2f3d28daaae33f83201ba7d74d7095521d7
vs1739562 <85999560+vs1739562@users.noreply.github.com>
2021-10-27 19:19:14 +0530

    Add files via upload

--- hermite poly.py
+++ hermite poly.py
@@ -9,7 +9,10 @@ m=1.
 w=1.
 hbar=1.
 
-
+import matplotlib.pyplot as plt
+from pylab import rcParams;
+rcParams['figure.figsize'] = 10, 10
+fig = plt.figure(figsize = (5,5))
 
 import numpy as np
 import numpy.polynomial.hermite as Herm
@@ -39,3 +42,68 @@ def stationary_state(x,n):
 #plt.ylabel(r"$\psi_4(x)$")
 #plt.title(r"Test Plot of $\psi_4(x)$")
 #plt.show()
+
+
+
+
+import scipy.integrate as integrate
+Cf_1 = np.zeros((5,5)) #first order transistion coefficient
+CF_1 = np.zeros(5)
+time = np.zeros(5)
+
+for i in range(5):
+    result = integrate.quad(lambda x: stationary_state(x, i+1)*stationary_state(x, 1),-50 ,50)
+    
+    #Cf_1[i] = result[0] ## this is just probability <f|V|i> where i took V = 1
+    #print(type(Cf_1[i]))
+    for t in range(5):
+        time[t]=t
+        a =( np.exp(complex(0,1)*(i-1)*w*t)-1)  
+        
+        if i != 0:
+            res1 = a /i ## this is a/(w-w(fi))
+            res = complex( result[1],0) *res1 ### this is  transistion probability for particular vallue of f(final state )
+            ## particular value of t 
+            
+            
+        
+            #print(abs(res))
+            CF_1[t] = res  ## transition coefficient with resoect to t having fixed f
+
+            Cf_1[i,t]= res
+            
+            
+colours = ['r','g','b','k']            
+for i in range(5):
+    fig = plt.figure(figsize = (5,5))
+
+    #ax1= fig.add_subplot(1,1,1)
+    plt.plot(time,Cf_1[:,i], label = "Transistion probability over time" )
+    plt.xlabel("time")
+    plt.ylabel(i )
+   
+    plt.legend(loc = "best",fancybox=True, shadow=True, prop={'size':'small'})  
+
+for i in range(5):
+    fig = plt.figure(figsize = (5,5))
+
+    #ax1= fig.add_subplot(1,1,1)
+    plt.plot(time,Cf_1[i,:], label = "Transistion probability with state" )
+    plt.xlabel("state")
+    plt.ylabel(i )
+   
+    plt.legend(loc = "best",fancybox=True, shadow=True, prop={'size':'small'})         
+          
+   
+#print(Cf_1)
+   
+#plt.figure()
+#plt.plot(time, CF_1)
+#plt.xlabel(r"x")
+#plt.ylabel(r"$\psi_4(x)$")
+#plt.title(r"Test Plot of $\psi_4(x)$")
+#plt.show() 
+   
+ 
+
+
